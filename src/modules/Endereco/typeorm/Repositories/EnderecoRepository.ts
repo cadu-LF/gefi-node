33ba6ebc80c6234e2bb5e1f5b2027ba6d363322d
Cadu Lourenço <nice.ice312@gmail.com>
2021-09-20 20:57:16 -0300

    Repositório de Enderecos criado, contedo métodos para buscar endereco por id, por rua ou por número

---
+++ src/modules/Endereco/typeorm/Repositories/EnderecoRepository.ts
@@ -0,0 +1,59 @@
+// repositório da entidade Product
+
+import { EntityRepository, Repository } from "typeorm";
+import Endereco from "../Entities/Endereco";
+
+@EntityRepository(Endereco)
+export default class ProductRepository extends Repository<Endereco>{
+
+  /**
+   * Busca Endereco pela rua
+   * 
+   * @param string: rua
+   * @return Promisse<Endereco | undefined>
+   */
+  public async findByRua(rua: string): Promise<Endereco | undefined> {
+    
+    const endereco = await this.findOne({
+      where: { 
+        rua
+      }
+    })
+
+    return endereco
+  }
+
+  /**
+   * Busca endereco por número
+   * 
+   * @param number: numero
+   * @return Promisse<Endereco | undefined>
+   */
+  public async findByNumero(numero: number): Promise<Endereco | undefined> {
+    
+    const endereco = await this.findOne({
+      where: { 
+        numero
+      }
+    })
+
+    return endereco
+  }
+
+  /**
+   * Busca Endereco pelo id
+   * 
+   * @param number: id
+   * @return Promisse<Endereco | undefined>
+   */
+  public async findById(id: number): Promise<Endereco | undefined> {
+    
+    const endereco = await this.findOne({
+      where: { 
+        id
+      }
+    })
+
+    return endereco
+  }
+}
