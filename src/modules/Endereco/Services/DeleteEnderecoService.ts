7104895fa96295d028a1cb0eb5787686cbcf87a1
Cadu Lourenço <nice.ice312@gmail.com>
2021-09-20 20:59:51 -0300

    Service para verificar se endereço existe e exclui-lo caso exista criada

---
+++ src/modules/Endereco/Services/DeleteEnderecoService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "../../../shared/errors/AppErrors";
+import EnderecoRepository from "../typeorm/Repositories/EnderecoRepository";
+
+interface IRequest {
+  id: number
+}
+
+export default class DeleteProductService {
+
+  public async execute({id}: IRequest): Promise<void> {
+
+    let enderecoRepository = getCustomRepository(EnderecoRepository);
+
+    let endereco = await enderecoRepository.findOne(id);
+
+    if(!endereco) {
+      throw new AppError(`Endereço com id: ${id} não existe`);
+    }
+    
+    await enderecoRepository.remove(endereco);
+  }
+}
