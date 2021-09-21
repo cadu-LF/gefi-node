becfde36c00b23b396b9b4f24789a97a87b287d4
Cadu Lourenço <nice.ice312@gmail.com>
2021-09-20 21:02:08 -0300

    Service para validar se o endereço existe a caso sim, mostrar esse endereço criada

---
+++ src/modules/Endereco/Services/ShowEnderecoService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from "typeorm"
+import AppError from "../../../shared/errors/AppErrors"
+import Endereco from "../typeorm/Entities/Endereco"
+import EnderecoRepository from "../typeorm/Repositories/EnderecoRepository"
+
+interface IRequest {
+  id: string
+}
+
+export default class ShowProductService {
+  
+  public async execute({id}: IRequest): Promise<Endereco> {
+
+    let enderecoRepository = getCustomRepository(EnderecoRepository);
+    let endereco = await enderecoRepository.findOne(id);
+
+    if(!endereco) {
+      throw new AppError(`Endreco com id: ${id} não existe`);
+    }
+    
+    return endereco;
+  }
+}
